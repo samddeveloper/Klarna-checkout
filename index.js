47cf54960d911b0b31ddc7a87b32cc98fc2cf05a
Sammie Slootman <158335988+samddeveloper@users.noreply.github.com>
2024-05-05 16:16:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/', async (req, res) => {
                 <div class="product">
                     <img src="${p.image}" alt="${p.title}">
                     <div style="text-align: center;">
-                        <a href="/products/${p.id}">
+                        <a href="/products/${p.id}" class="product-link">
                             <h3>${p.title}</h3>
                             <p>Price: ${p.price} kr</p>
                         </a>
